a61be932658d52b02bb7b809fbfdd9c544df3ba1
Olivia <laconte.olivia@sfr.fr>
2023-02-01 13:08:55 +0100

    structure of the game

--- src/game/TicTacToe.java
+++ src/game/TicTacToe.java
@@ -1,4 +1,29 @@
 package game;
 
-public class TicTacToe {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TicTacToe implements ActionListener {
+    TicTacToe() {
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+    public void firstTurn() {
+
+    }
+
+    public void check() {
+
+    }
+
+    public void xWins (int a, int b, int c) {
+
+    }
+
+    public void oWins (int a, int b, int c) {
+
+    }
 }
